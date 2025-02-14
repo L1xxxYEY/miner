688a41cdd80bd8ed16d9da5cc38dbfeef059572e
L1xxxYEY <sergeevamarinaaaa2004@gmail.com>
2025-02-14 20:03:09 +0500

    ready

--- main.js
+++ main.js
@@ -39,32 +39,6 @@ async function auth() {
   }
 }
 
-// Функция для выбора баллов
-function addPoint(event) {
-  let target = event.target.innerHTML;
-  POINT = +target;
-  let activePoints = document.querySelector(".point.active");
-  if (activePoints) {
-    activePoints.classList.remove("active");
-  }
-  event.target.classList.add("active");
-}
-
-// Функция для обновления баланса пользователя
-async function updateUSerBalance() {
-  let response = await sendRequest("user", "GET", {
-    username: USERNAME,
-  });
-
-  if (response.error) {
-    //Если есть ошибка
-    alert(response.message);
-  } else {
-    const user = document.querySelector("header span");
-    user.innerHTML = `Пользователь ${response.username} с балансом ${response.balance}`;
-  }
-}
-
 // Функция для кнопки начала игры
 function startOrStopGame() {
   if (gameBtn.innerHTML === "ИГРАТЬ") {
@@ -74,6 +48,7 @@ function startOrStopGame() {
   } else {
     gameBtn.innerHTML = "ИГРАТЬ";
     gameBtn.style.backgroundColor = "#66a663";
+    stopGame();
   }
 }
 
@@ -94,22 +69,138 @@ async function startGame() {
   }
 }
 
+// Функция для завершения игры
+async function stopGame() {
+  const response = await sendRequest("stop_game", "POST", {
+    username: USERNAME,
+    game_id,
+  });
+  updateUSerBalance();
+  resetField();
+}
+
 //Функция для активации игрового поля
 function activateArea() {
   let field = document.getElementsByClassName("field");
+
   for (let i = 0; i < field.length; i++) {
+    const row = Math.trunc(i / 10);
+    const column = i - row * 10;
     field[i].addEventListener("contextmenu", setFlag);
+    field[i].setAttribute("data-row", row);
+    field[i].setAttribute("data-column", column);
+    field[i].addEventListener("click", makeStep);
+
     setTimeout(() => {
       field[i].classList.add("active");
     }, i * 30);
   }
 }
 
+async function makeStep(event) {
+  const target = event.target;
+  const row = +target.getAttribute("data-row");
+  const column = +target.getAttribute("data-column");
+  try {
+    const response = await sendRequest("game_step", "POST", {
+      game_id,
+      row: row,
+      column: column,
+    });
+    updateArea(response.table);
+    if (response.error) {
+      alert(response.message);
+    } else {
+      if (response.status === "Ok") {
+      } else if (response.status === "Failed") {
+        alert("Вы проиграли");
+        updateUSerBalance();
+        gameBtn.innerHTML = "ИГРАТЬ";
+        gameBtn.style.backgroundColor = "#66a663";
+        setTimeout(() => {
+          resetField();
+        }, 2000);
+      } else if (response.status === "Won") {
+        alert("Вы победитель");
+        updateUSerBalance();
+        gameBtn.innerHTML = "ИГРАТЬ";
+        gameBtn.style.backgroundColor = "#66a663";
+        setTimeout(() => {
+          resetField();
+        }, 2000);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function updateArea(table) {
+  let fields = document.querySelectorAll(".field");
+  let a = 0;
+  for (let i = 0; i < table.length; i++) {
+    let row = table[i];
+    for (let j = 0; j < row.length; j++) {
+      let cell = row[j];
+      let value = fields[a];
+      if (cell === "") {
+      } else if (cell === 0) {
+        value.classList.remove("active");
+      } else if (cell == "BOMB") {
+        value.classList.remove("active");
+        value.classList.add("bomb");
+      } else if (cell > 0) {
+        value.classList.remove("active");
+        value.innerHTML = cell;
+      }
+      a++;
+    }
+  }
+}
+
+// Функция для обновления баланса пользователя
+async function updateUSerBalance() {
+  let response = await sendRequest("user", "GET", {
+    username: USERNAME,
+  });
+
+  if (response.error) {
+    //Если есть ошибка
+    alert(response.message);
+  } else {
+    const user = document.querySelector("header span");
+    user.innerHTML = `Пользователь ${response.username} с балансом ${response.balance}`;
+  }
+}
+
+function resetField() {
+  const gameField = document.querySelector(".gameField");
+  gameField.innerHTML = "";
+  for (let i = 0; i < 80; i++) {
+    const field = document.createElement("div");
+    field.classList.add("field");
+    gameField.appendChild(field);
+  }
+}
+
+resetField();
+
 //Функция для добавления иконки флажка
 function setFlag(event) {
   event.preventDefault();
   let target = event.target;
   target.classList.toggle("flag");
+}
+
+// Функция для выбора баллов
+function addPoint(event) {
+  let target = event.target.innerHTML;
+  POINT = +target;
+  let activePoints = document.querySelector(".point.active");
+  if (activePoints) {
+    activePoints.classList.remove("active");
+  }
+  event.target.classList.add("active");
 }
 
 async function sendRequest(url, method, data) {
